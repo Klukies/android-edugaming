0a4ade442851ef6538a7ad185371322b835cddef
Cornille Lukas <lukas.cornille@student.howest.be>
2019-03-16 11:42:50 +0100

    finished sign-up activity todo: api connection & authentication

---
+++ app/src/main/java/be/howest/nma/SignUpActivity.java
@@ -0,0 +1,18 @@
+package be.howest.nma;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+    public void backToLogin(View view) {
+        finish();
+    }
+}
