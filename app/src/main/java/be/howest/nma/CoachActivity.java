8f1194248a2c44cb371dbcbcb976946412dff455
Cornille Lukas <lukascornille@hotmail.com>
2019-03-20 16:17:32 +0100

    Logout working

--- app/src/main/java/be/howest/nma/CoachActivity.java
+++ app/src/main/java/be/howest/nma/CoachActivity.java
@@ -1,7 +1,12 @@
 package be.howest.nma;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CoachActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class CoachActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coach);
+    }
+
+    public void logout(View view) {
+        SharedPreferences preferences = this.getSharedPreferences("authentication", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.remove(getString(R.string.accessToken));
+        editor.apply();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
